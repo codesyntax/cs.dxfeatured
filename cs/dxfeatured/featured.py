8a567718a829e9ddfe1011509c900b07e74d9eb1
Lur Ibargutxi <libargutxi@codesyntax.com>
2016-03-14 15:24:21 +0100

    richtext

--- cs/dxfeatured/featured.py
+++ cs/dxfeatured/featured.py
@@ -1,24 +1,12 @@
-from five import grok
-from plone.app.textfield import RichText
+from Products.Five.browser import BrowserView
 from plone.directives import dexterity
 from plone.directives import form
-from plone.formwidget.contenttree import ObjPathSourceBinder
-from plone.namedfile.field import NamedBlobFile
 from plone.namedfile.field import NamedBlobImage
-from plone.namedfile.field import NamedFile
-from plone.namedfile.field import NamedImage
 from plone.namedfile.interfaces import IImageScaleTraversable
-from z3c.form import field
-from z3c.form import group
-from z3c.relationfield.schema import RelationChoice
-from z3c.relationfield.schema import RelationList
 from zope import schema
-from zope.interface import Invalid
-from zope.interface import invariant
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleTerm
-from zope.schema.vocabulary import SimpleVocabulary
 from cs.dxfeatured import MessageFactory as _
+from zope.interface import implements
+from plone.app.textfield import RichText
 
 
 # Interface class; used to define content-type schema.
@@ -43,12 +31,16 @@ class IFeatured(form.Schema, IImageScaleTraversable):
         required=True,
         )
 
+    text = RichText(title=_(u'Featured text'),
+        required=False,
+        )
 # Custom content-type class; objects created for this content type will
 # be instances of this class. Use this class to add content-type specific
 # methods and properties. Put methods that are mainly useful for rendering
 # in separate view classes.
+
 class Featured(dexterity.Item):
-    grok.implements(IFeatured)
+    implements(IFeatured)
     # Add your class methods and properties here
 
 
@@ -60,10 +52,9 @@ class Featured(dexterity.Item):
 # interface with "/@@view" appended unless specified otherwise
 # using grok.name below.
 # This will make this view the default view for your content-type
-grok.templatedir('templates')
 
 
-class FeaturedView(grok.View):
-    grok.context(IFeatured)
-    grok.require('zope2.View')
-    grok.name('view')
+class FeaturedView(BrowserView):
+    """
+    The view
+    """
